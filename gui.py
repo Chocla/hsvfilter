208984334416d19b748bed5ee2c4392bdef7260e
Chocla <19178866+Chocla@users.noreply.github.com>
2019-06-13 14:59:14 -0700

    Add sliders

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget,QSlider
 from PyQt5.QtCore import QSize  
 
 class Window(QMainWindow):
@@ -18,3 +18,13 @@ class Window(QMainWindow):
         title = QLabel("Hello World ",self)
         title.setAlignment(QtCore.Qt.AlignCenter)
         gridLayout.addWidget(title,0,0)
+
+        #TODO: (Joe) Find double sided sliders to use instead of these
+        #TODO: (Joe) Connect Sliders with variables that control HSV ranges
+        #TODO: (Joe) Add Labels for the sliders
+        slider1 = QSlider(QtCore.Qt.Horizontal)
+        gridLayout.addWidget(slider1,1,0)
+        slider2 = QSlider(QtCore.Qt.Horizontal)
+        gridLayout.addWidget(slider2,1,1)
+        slider3 = QSlider(QtCore.Qt.Horizontal)
+        gridLayout.addWidget(slider3,1,2)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 import gui
 
-
+#TODO: (Joe) Write HSV Filter (lol)
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mainWin = gui.Window()
