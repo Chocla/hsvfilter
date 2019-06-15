ac03df26b1c998d96ac5a6684bb60c4e9566e054
Chocla <19178866+Chocla@users.noreply.github.com>
2019-06-15 12:53:26 -0700

    Add range sliders

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget,QSlider
 from PyQt5.QtCore import QSize  
+from qrangeslider import QRangeSlider
 
 class Window(QMainWindow):
     def __init__(self):
@@ -19,12 +20,22 @@ class Window(QMainWindow):
         title.setAlignment(QtCore.Qt.AlignCenter)
         gridLayout.addWidget(title,0,0)
 
-        #TODO: (Joe) Find double sided sliders to use instead of these
         #TODO: (Joe) Connect Sliders with variables that control HSV ranges
-        #TODO: (Joe) Add Labels for the sliders
-        slider1 = QSlider(QtCore.Qt.Horizontal)
-        gridLayout.addWidget(slider1,1,0)
-        slider2 = QSlider(QtCore.Qt.Horizontal)
-        gridLayout.addWidget(slider2,1,1)
-        slider3 = QSlider(QtCore.Qt.Horizontal)
-        gridLayout.addWidget(slider3,1,2)
+        
+        self.slider1 = QRangeSlider()
+        self.slider1.setFixedHeight(15)
+        self.slider2 = QRangeSlider()
+        self.slider2.setFixedHeight(15)
+        self.slider3 = QRangeSlider()
+        self.slider3.setFixedHeight(15)
+        gridLayout.addLayout(self.setupSlider(self.slider1, QLabel("Hue")),1,0)
+        gridLayout.addLayout(self.setupSlider(self.slider2, QLabel("Saturation")),1,1)
+        gridLayout.addLayout(self.setupSlider(self.slider3, QLabel("Value")),1,2)
+
+    def setupSlider(self,slider, label):
+        slider.setMin(0)
+        slider.setMax(255)
+        tmp = QGridLayout(self)
+        tmp.addWidget(label,0,0)
+        tmp.addWidget(slider,1,0)
+        return tmp
