fix angle calculation

gui.py
@@ -36,6 +36,7 @@ class vidThread(QThread):
                     maxContour = self.findBiggestContour(contours)
                     rx,ry,rw,rh = cv2.boundingRect(maxContour)
                     cv2.rectangle(frame,(rx,ry), (rx+rw,ry+rh),(0,0,255))
+                    cv2.circle(frame, (rx + int(rw/2), ry+int(rh/2)),5, (0,0,255))
                 self.calculateDirection(rx,ry,rw,rh,frame.shape)
                 #cv2.rectangle(res, (50,50),(100,100), (255,0,0), 2)
                 h, w, ch = res.shape
@@ -49,17 +50,13 @@ class vidThread(QThread):
                 self.changePixmap.emit(final)
     #calculates the direction the camera needs to move in order to center the detected object
     def calculateDirection(self,x,y,h,w,shape):
-        vec = (x + (w/2) - shape[0]/2, y + (h/2) - shape[1]/2)
-        try:
-            theta = np.arctan(vec[1]/vec[0])*(180/np.pi)
-            if vec[0] < 0:
-                theta += 180    
-        except ZeroDivisionError:
-            theta = 90 if vec[1] > 0 else 270
-                
-        print(int( (theta - 22.5) / 45))
-        #if length of vector is short enough, don't display an arrow
-        #else 
+        vec = (x + (w/2) - (shape[1]/2), -(y + (h/2)) +(shape[0]/2))
+      
+        theta = np.arctan2([vec[1]],[vec[0]])[0]*(180/np.pi)
+        if theta < 0:
+            theta += 360
+        
+        #TODO: Draw Arrows on screen depending on region
         pass
     def findBiggestContour(self, contours):
         maxArea = 0